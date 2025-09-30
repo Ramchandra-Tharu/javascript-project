474799376fa753931fd7e086c9b6ea0a929cd540
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-09-30 22:01:13 +0530

    new features is added

--- prject/script.js
+++ prject/script.js
@@ -1,28 +1,31 @@
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
-const buttons = document.querySelectorAll('.button')
-buttons.forEach(function(button)
-{
-button.addEventListener('click', (e)=>{
-    if(e.target.id ==='gray'){
-        body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id ==='red'){
-        body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id === 'Green'){
-        body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id === 'yellow'){
-        body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id ==='blue')
-    {
-        body.style.backgroundColor =e.target.id
-    }
+const buttons = document.querySelectorAll('.button');
+const refresh = document.querySelector('.refresh');
 
-   
-})
-}
+buttons.forEach(function (button) {
+    button.addEventListener('click', (e) => {
+        if (e.target.id === 'gray') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'Green') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id ==='orange'){
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
 
-)
+refresh.addEventListener('click', () => {
+    body.style.backgroundColor = '';
+});
