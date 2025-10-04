eecdf2a7f1c32a22a5fd09e830b631f8a1648a61
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-10-04 15:56:37 +0530

    js is added

---
+++ project_3/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#result')
+
+    if (height === '' || height <= 0 || isNaN(height)){
+ result.innerHTML = `please give a valid height ${height}`;
+    }
+       
+
+else if (weight === '' || weight <= 0 || isNaN(weight)) {
+    result.innerHTML = `Please give a valid height ${weight}`;
+}
+
+
+else{
+    const bmi =(weight / ((height/100) * (height/100))).toFixed(2);
+    result.innerHTML = `your bmi is ${bmi}`
+}
+}
+
+
+)
